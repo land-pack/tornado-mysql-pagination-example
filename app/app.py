8385962d7a5945a1a13d3f4326047d95b1b00ef2
Frank AK <frank@localhost.localdomain>
2016-10-17 07:26:17 -0400

    simple pagination

--- app/app.py
+++ app/app.py
@@ -12,15 +12,34 @@ def gen_many_data(num=1000):
         language = fy.personal.language()
         yield (i, name, addr, language)
 
+def gen_page(num=1000, page=10):
+    a = []
+    for j in range(0,num,page):
+        d = gen_many_data(page)
+        a.append(d)
+    return a
+
+
+
 class IndexHandler(web.RequestHandler):
     def get(self):
+
         data = gen_many_data()
         self.render("index.html", data=data)
 
+class PaginationHandler(web.RequestHandler):
+    def get(self):
+        page = self.get_argument('page')
+        data = gen_page(1000)
+        page = int(page)
+        page_data = data[page]
+        max_page = 100
+        self.render('page.html', data=page_data, page=page, max=max_page)        
 
 if __name__ == '__main__':
     application = web.Application([
             (r'/', IndexHandler),
+            (r'/page', PaginationHandler),
         ],
         template_path = os.path.join(os.path.dirname(__file__),'templates'),
         static_path = os.path.join(os.path.dirname(__file__),'static'),
